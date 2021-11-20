622ebee5afef54aeca4a7ff9ed0bec92724f9993
Yaroslav Mytkalyk <docd1990@gmail.com>
2021-11-20 14:20:54 +0200

    Rename HomeActivity to MainActivity

--- app/src/main/java/com/doctoror/splittor/presentation/main/MainActivity.kt
+++ app/src/main/java/com/doctoror/splittor/presentation/main/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.doctoror.splittor.presentation.home
+package com.doctoror.splittor.presentation.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -7,16 +7,16 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.doctoror.splittor.R
-import com.doctoror.splittor.databinding.ActivityHomeBinding
+import com.doctoror.splittor.databinding.ActivityMainBinding
 
-class HomeActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding = ActivityHomeBinding.inflate(layoutInflater)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
 
