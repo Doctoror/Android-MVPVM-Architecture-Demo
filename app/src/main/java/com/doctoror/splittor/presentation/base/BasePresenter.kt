75b2ddebda3f4124ea7003de4e84d495fdbfe156
Yaroslav Mytkalyk <docd1990@gmail.com>
2021-12-03 22:18:12 +0200

    Remove disposeOnDestroy functionality from BasePresenter

--- app/src/main/java/com/doctoror/splittor/presentation/base/BasePresenter.kt
+++ app/src/main/java/com/doctoror/splittor/presentation/base/BasePresenter.kt
@@ -1,27 +1,17 @@
 package com.doctoror.splittor.presentation.base
 
-import androidx.lifecycle.*
-import io.reactivex.rxjava3.disposables.CompositeDisposable
-import io.reactivex.rxjava3.disposables.Disposable
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.ViewModel
 
 abstract class BasePresenter : ViewModel(), LifecycleEventObserver {
 
-    private val disposables = CompositeDisposable()
-
     override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
-        when (event) {
-            Lifecycle.Event.ON_CREATE -> onCreate()
-            Lifecycle.Event.ON_DESTROY -> onDestroy()
+        if (event == Lifecycle.Event.ON_CREATE) {
+            onCreate()
         }
     }
 
     abstract fun onCreate()
-
-    fun onDestroy() {
-        disposables.clear()
-    }
-
-    fun Disposable.disposeOnDestroy() {
-        disposables.add(this)
-    }
 }
