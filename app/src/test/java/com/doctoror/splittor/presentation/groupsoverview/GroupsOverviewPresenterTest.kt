33b9dd1ce36cb4a5fe85508ea09a012508ff5835
Yaroslav Mytkalyk <docd1990@gmail.com>
2023-05-16 13:43:04 +0300

    Use Dispatchers.setMain instead of replacing ViewModelScope

--- app/src/test/java/com/doctoror/splittor/presentation/groupsoverview/GroupsOverviewPresenterTest.kt
+++ app/src/test/java/com/doctoror/splittor/presentation/groupsoverview/GroupsOverviewPresenterTest.kt
@@ -2,15 +2,21 @@ package com.doctoror.splittor.presentation.groupsoverview
 
 import com.doctoror.splittor.domain.groups.Group
 import com.doctoror.splittor.domain.groups.ObserveGroupsUseCase
-import com.doctoror.splittor.presentation.base.runInViewModelScopeBlocking
+import com.doctoror.splittor.presentation.base.MainDispatcherRule
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.flowOf
+import org.junit.Rule
 import org.junit.Test
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
 
 class GroupsOverviewPresenterTest {
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @get:Rule
+    val mainDispatcherRule = MainDispatcherRule()
 
     private val observeGroupsUseCase: ObserveGroupsUseCase = mock()
     private val viewModelUpdater: GroupsOverviewViewModelUpdater = mock()
@@ -26,7 +32,7 @@ class GroupsOverviewPresenterTest {
         val groups = listOf<Group>(mock())
         whenever(observeGroupsUseCase.observe()).thenReturn(flowOf(groups))
 
-        underTest.runInViewModelScopeBlocking { underTest.onCreate() }
+        underTest.onCreate()
 
         verify(viewModelUpdater).updateOnGroupsListLoaded(groups)
     }
