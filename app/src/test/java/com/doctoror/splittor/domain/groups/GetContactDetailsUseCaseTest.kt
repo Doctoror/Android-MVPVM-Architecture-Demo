85193a83ad193be1380c1b774e6c2c964bbeb671
Yaroslav Mytkalyk <docd1990@gmail.com>
2023-05-16 13:15:00 +0300

    Add GetContactDetailsUseCaseTest

---
+++ app/src/test/java/com/doctoror/splittor/domain/groups/GetContactDetailsUseCaseTest.kt
@@ -0,0 +1,27 @@
+package com.doctoror.splittor.domain.groups
+
+import android.net.Uri
+import com.doctoror.splittor.domain.contacts.ContactDetails
+import com.doctoror.splittor.domain.contacts.ContactDetailsRepository
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
+import java.util.Optional
+
+class GetContactDetailsUseCaseTest {
+
+    private val contactDetailsRepository: ContactDetailsRepository = mock()
+
+    private val underTest = GetContactDetailsUseCase(contactDetailsRepository)
+
+    @Test
+    fun getsForUri() = runTest {
+        val uri: Uri = mock()
+        val output = Optional.of(mock<ContactDetails>())
+        whenever(contactDetailsRepository.getForUri(uri)).thenReturn(output)
+
+        assertEquals(output, underTest.getForUri(uri))
+    }
+}
