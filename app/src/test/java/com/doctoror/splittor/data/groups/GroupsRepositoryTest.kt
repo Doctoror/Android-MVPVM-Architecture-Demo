8f6bfa3aeb346d2594b2f056a7dbdbaa614cc44e
Yaroslav Mytkalyk <docd1990@gmail.com>
2023-05-16 13:22:26 +0300

    Replace runBlocking with runTest

--- app/src/test/java/com/doctoror/splittor/data/groups/GroupsRepositoryTest.kt
+++ app/src/test/java/com/doctoror/splittor/data/groups/GroupsRepositoryTest.kt
@@ -2,7 +2,7 @@ package com.doctoror.splittor.data.groups
 
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.single
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.mockito.kotlin.mock
@@ -15,10 +15,10 @@ class GroupsRepositoryTest {
     private val underTest = GroupsRepositoryImpl(groupsDataSource)
 
     @Test
-    fun observeEmitsDataFromDataSource() {
+    fun observeEmitsDataFromDataSource() = runTest {
         val data = listOf<GroupWithMembers>(mock())
         whenever(groupsDataSource.observe()).thenReturn(flowOf(data))
 
-        runBlocking { assertEquals(data, underTest.observe().single()) }
+        assertEquals(data, underTest.observe().single())
     }
 }
--- app/src/test/java/com/doctoror/splittor/data/groups/LocalGroupsDataSourceTest.kt
+++ app/src/test/java/com/doctoror/splittor/data/groups/LocalGroupsDataSourceTest.kt
@@ -2,7 +2,7 @@ package com.doctoror.splittor.data.groups
 
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.single
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.mockito.kotlin.mock
@@ -15,10 +15,10 @@ class LocalGroupsDataSourceTest {
     private val underTest = LocalGroupsDataSource(groupsDao)
 
     @Test
-    fun observeGroupsWithMembersEmitsDataFromDao() {
+    fun observeGroupsWithMembersEmitsDataFromDao() = runTest {
         val data = listOf<GroupWithMembers>(mock())
         whenever(groupsDao.observeGroupsWithMembers()).thenReturn(flowOf(data))
 
-        runBlocking { assertEquals(data, underTest.observe().single()) }
+        assertEquals(data, underTest.observe().single())
     }
 }
