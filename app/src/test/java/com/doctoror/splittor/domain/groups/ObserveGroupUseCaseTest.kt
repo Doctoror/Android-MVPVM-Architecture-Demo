72d6d437d2e54239674e714ed4036e9b77cb1f97
Yaroslav Mytkalyk <docd1990@gmail.com>
2023-05-16 13:09:24 +0300

    Add ObserveGroupUseCaseTest

---
+++ app/src/test/java/com/doctoror/splittor/domain/groups/ObserveGroupUseCaseTest.kt
@@ -0,0 +1,25 @@
+package com.doctoror.splittor.domain.groups
+
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.single
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
+
+class ObserveGroupUseCaseTest {
+
+    private val groupsRepository: GroupsRepository = mock()
+
+    private val underTest = ObserveGroupUseCase(groupsRepository)
+
+    @Test
+    fun observeEmitsDataFromRepository() = runTest {
+        val data: Group = mock()
+        val id = 12L
+        whenever(groupsRepository.observe(id)).thenReturn(flowOf(data))
+
+        assertEquals(data, underTest.observe(id).single())
+    }
+}
