b7babaa3324f7210d2d1f390976a4202db1df397
Yaroslav Mytkalyk <docd1990@gmail.com>
2023-05-16 13:12:22 +0300

    Add UpdateMemberPaidStatusUseCaseTest

---
+++ app/src/test/java/com/doctoror/splittor/domain/groups/UpdateMemberPaidStatusUseCaseTest.kt
@@ -0,0 +1,23 @@
+package com.doctoror.splittor.domain.groups
+
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
+
+class UpdateMemberPaidStatusUseCaseTest {
+
+    private val groupsRepository: GroupsRepository = mock()
+
+    private val underTest = UpdateMemberPaidStatusUseCase(groupsRepository)
+
+    @Test
+    fun updatesMemberPaidStatus() = runTest {
+        val memberId = 64L
+        val paid = true
+
+        underTest.updateMemberPaidStatus(memberId, paid)
+
+        verify(groupsRepository).updateMemberPaidStatus(memberId, paid)
+    }
+}
