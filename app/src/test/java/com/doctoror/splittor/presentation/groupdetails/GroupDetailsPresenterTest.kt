05e3575ff0aa30a7a05b5541b0e9bd932c835bcd
Yaroslav Mytkalyk <docd1990@gmail.com>
2023-05-17 18:13:39 +0300

    Add GroupDetailsPresenterTest

---
+++ app/src/test/java/com/doctoror/splittor/presentation/groupdetails/GroupDetailsPresenterTest.kt
@@ -0,0 +1,55 @@
+package com.doctoror.splittor.presentation.groupdetails
+
+import com.doctoror.splittor.domain.groups.Group
+import com.doctoror.splittor.domain.groups.ObserveGroupUseCase
+import com.doctoror.splittor.domain.groups.UpdateMemberPaidStatusUseCase
+import com.doctoror.splittor.presentation.base.MainDispatcherRule
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+
+class GroupDetailsPresenterTest {
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @get:Rule
+    val mainDispatcherRule = MainDispatcherRule()
+
+    private val groupId = 1923L
+    private val observeGroupUseCase: ObserveGroupUseCase = mock()
+    private val updateMemberPaidStatusUseCase: UpdateMemberPaidStatusUseCase = mock()
+    private val viewModelUpdater: GroupDetailsViewModelUpdater = mock()
+
+    private val underTest = GroupDetailsPresenter(
+        Dispatchers.Unconfined,
+        groupId,
+        observeGroupUseCase,
+        updateMemberPaidStatusUseCase,
+        viewModelUpdater
+    )
+
+    @Test
+    fun observesGroupAndUpdatesViewModelOnCreate() {
+        val group: Group = mock()
+        whenever(observeGroupUseCase.observe(groupId)).thenReturn(flowOf(group))
+
+        underTest.onCreate()
+
+        verify(viewModelUpdater).update(group)
+    }
+
+    @Test
+    fun updatesMemberPaidStatus() = runTest {
+        val memberId = 84L
+        val paid = true
+
+        underTest.updateMemberPaidStatus(memberId, paid)
+
+        verify(updateMemberPaidStatusUseCase).updateMemberPaidStatus(memberId, paid)
+    }
+}
