85daa633ef32d201c12fb706892a6c44decc0562
Yaroslav Mytkalyk <docd1990@gmail.com>
2023-05-17 13:13:27 +0300

    Complete GroupsRepositoryTest

--- app/src/test/java/com/doctoror/splittor/data/groups/GroupsRepositoryTest.kt
+++ app/src/test/java/com/doctoror/splittor/data/groups/GroupsRepositoryTest.kt
@@ -1,11 +1,13 @@
 package com.doctoror.splittor.data.groups
 
+import com.doctoror.splittor.domain.contacts.ContactDetails
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.single
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
 
 class GroupsRepositoryTest {
@@ -15,10 +17,43 @@ class GroupsRepositoryTest {
     private val underTest = GroupsRepositoryImpl(groupsDataSource)
 
     @Test
+    fun insertsIntoDataSource() = runTest {
+        val contacts: List<ContactDetails> = listOf(mock())
+        val amount = "34343.12"
+        val title = "Some title"
+
+        val insertedGroupId = 125L
+        whenever(groupsDataSource.insert(contacts, amount, title)).thenReturn(insertedGroupId)
+
+        val output = underTest.insert(contacts, amount, title)
+
+        assertEquals(insertedGroupId, output)
+    }
+
+    @Test
     fun observeEmitsDataFromDataSource() = runTest {
         val data = listOf<GroupWithMembers>(mock())
         whenever(groupsDataSource.observe()).thenReturn(flowOf(data))
 
         assertEquals(data, underTest.observe().single())
+    }
+
+    @Test
+    fun observeByIdEmitsDataFromDataSource() = runTest {
+        val id = 3434L
+        val data: GroupWithMembers = mock()
+        whenever(groupsDataSource.observe(id)).thenReturn(flowOf(data))
+
+        assertEquals(data, underTest.observe(id).single())
+    }
+
+    @Test
+    fun updatesMemberPaidStatusInDataSource() = runTest {
+        val memberId = 134L
+        val paid = false
+
+        underTest.updateMemberPaidStatus(memberId, paid)
+
+        verify(groupsDataSource).updateMemberPaidStatus(memberId, paid)
     }
 }
