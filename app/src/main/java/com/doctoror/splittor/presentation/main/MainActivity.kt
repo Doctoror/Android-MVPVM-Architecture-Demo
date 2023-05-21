ef0e9f1e19a69a8daaa1be17572f80dc379be61c
Yaroslav Mytkalyk <docd1990@gmail.com>
2023-05-21 13:26:12 +0300

    Remove onSupportNavigateUp from MainActivity

--- app/src/main/java/com/doctoror/splittor/presentation/main/MainActivity.kt
+++ app/src/main/java/com/doctoror/splittor/presentation/main/MainActivity.kt
@@ -2,7 +2,6 @@ package com.doctoror.splittor.presentation.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
 import com.doctoror.splittor.R
 
 class MainActivity : AppCompatActivity() {
@@ -10,9 +9,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        return findNavController(R.id.navHostFragment).navigateUp() || super.onSupportNavigateUp()
     }
 }
