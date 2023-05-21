4b6b3866d2b51616be5625c29fc0abfc4659b588
Yaroslav Mytkalyk <docd1990@gmail.com>
2023-05-21 16:20:54 +0300

    Migrate build.gradle to Kotlin DSL

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,15 +6,15 @@ buildscript {
     }
     dependencies {
 
-        classpath libs.kotlin.gradle.plugin
-        classpath libs.navigation.safe.args.gradle.plugin
-        classpath libs.tools.build.gradle
+        classpath(libs.kotlin.gradle.plugin)
+        classpath(libs.navigation.safe.args.gradle.plugin)
+        classpath(libs.tools.build.gradle)
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
