4c19a7204f3679f299ce42bd297ed9012cbe1af8
Yaroslav Mytkalyk <docd1990@gmail.com>
2023-05-21 16:30:16 +0300

    Use Kotlin DSL for data build.gradle

---
+++ data/build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    id("com.android.library")
+    id("kotlin-kapt")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.doctoror.splittor.data"
+
+    compileSdk = libs.versions.androidCompileSdkVersion.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.androidMinSdkVersion.get().toInt()
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"))
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+dependencies {
+    implementation(project(":domain"))
+
+    kapt(libs.room.compiler)
+
+    implementation(libs.room.ktx)
+    implementation(libs.room.runtime)
+    implementation(libs.koin)
+
+    testImplementation(libs.junit)
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.mockito.kotlin)
+
+    androidTestImplementation(libs.test.core)
+    androidTestImplementation(libs.test.runner)
+}
