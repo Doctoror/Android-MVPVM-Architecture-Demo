d4d0e6f1b0675f076b6f2d3183ae68fd1f5434da
Yaroslav Mytkalyk <docd1990@gmail.com>
2023-08-22 14:19:24 +0200

    Use Foreign Key for GroupMemberEntity

--- data/src/main/java/com/doctoror/splittor/data/groups/GroupMemberEntity.kt
+++ data/src/main/java/com/doctoror/splittor/data/groups/GroupMemberEntity.kt
@@ -1,11 +1,23 @@
 package com.doctoror.splittor.data.groups
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 
 const val GROUP_MEMBER_TABLE_NAME = "groupMembers"
 
-@Entity(tableName = GROUP_MEMBER_TABLE_NAME)
+@Entity(
+    tableName = GROUP_MEMBER_TABLE_NAME,
+    foreignKeys = [
+        ForeignKey(
+            entity = GroupEntity::class,
+            parentColumns = ["groupId"],
+            childColumns = ["groupMemberGroupId"],
+            onDelete = ForeignKey.CASCADE,
+            onUpdate = ForeignKey.CASCADE
+        )
+    ]
+)
 internal data class GroupMemberEntity(
     @PrimaryKey(autoGenerate = true) val groupMemberId: Long,
     val groupMemberGroupId: Long,
