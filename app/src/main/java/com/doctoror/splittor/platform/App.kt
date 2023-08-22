b35a041c2accd52cce02fcbc3eeb0790dab4ee37
Yaroslav Mytkalyk <docd1990@gmail.com>
2023-08-22 14:42:09 +0200

    Enable StrictMode

--- app/src/main/java/com/doctoror/splittor/platform/App.kt
+++ app/src/main/java/com/doctoror/splittor/platform/App.kt
@@ -1,7 +1,33 @@
 package com.doctoror.splittor.platform
 
 import android.app.Application
+import android.os.StrictMode
+import com.doctoror.splittor.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class App : Application()
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        initStrictMode()
+    }
+
+    private fun initStrictMode() {
+        if (BuildConfig.DEBUG) {
+            StrictMode.setThreadPolicy(
+                StrictMode.ThreadPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .build()
+            )
+
+            StrictMode.setVmPolicy(
+                StrictMode.VmPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .build()
+            )
+        }
+    }
+}
