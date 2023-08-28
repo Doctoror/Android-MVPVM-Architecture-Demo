e2e161ff483b1e9afe71d8e77acb466b67710f2b
Yaroslav Mytkalyk <docd1990@gmail.com>
2023-08-28 22:56:40 +0200

    Remove unnecessary configuration from top-level build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,19 +8,7 @@ plugins {
 }
 
 buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
     dependencies {
-
         classpath(libs.navigation.safe.args.gradle.plugin)
-
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
     }
-}
-
-tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
 }
