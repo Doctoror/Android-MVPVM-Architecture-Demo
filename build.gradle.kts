1e36e0bec8c7a058de6cfbe0860c5d35459b3f55
Yaroslav Mytkalyk <docd1990@gmail.com>
2023-08-28 22:49:19 +0200

    Rename toolsGradle to agp

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version libs.versions.toolsGradle apply false
-    id("com.android.library") version libs.versions.toolsGradle apply false
+    id("com.android.application") version libs.versions.agp apply false
+    id("com.android.library") version libs.versions.agp apply false
     id("com.google.dagger.hilt.android") version libs.versions.hilt apply false
     id("com.google.devtools.ksp") version libs.versions.ksp apply false
     id("org.jetbrains.kotlin.android") version libs.versions.kotlin apply false
